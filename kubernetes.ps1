905da5bc593cf17102badff8f2bec25330a145a4
danielhpt <38636029+danielhpt@users.noreply.github.com>
2021-06-22 22:19:09 +0100

    Done

--- kubernetes.ps1
+++ kubernetes.ps1
@@ -5,6 +5,7 @@ kubectl apply -f kubernetes/persistent-volume.yml
 kubectl apply -f kubernetes/persistent-volume-claim.yml
 kubectl apply -f kubernetes/statefulset.yml
 kubectl apply -f kubernetes/statefulset-service.yml
+Start-Sleep -s 15
 kubectl apply -f kubernetes/deployment-api.yml
 kubectl apply -f kubernetes/deployment-web.yml
 kubectl apply -f kubernetes/load-balancer-api.yml
